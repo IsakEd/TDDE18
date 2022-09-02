6550b80a9556518902d825f41e64a0b181d13ef4
jakob <jakob@DESKTOP-SCI93QV>
2022-09-02 09:15:38 +0200

    Add project files.

---
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,66 @@
+// ConsoleApplication1.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+#include <cstdio>
+#include <iostream>
+#include <iomanip>
+
+using namespace std;
+
+int main()
+
+
+
+{
+    int a;
+    std::string b;
+    double c;
+
+
+    cout << "\nEnter one integer: ";
+    cin >> a;
+    std::cout << "You entered the number: " ;
+    std::cout << a;
+   
+
+    std::cout << "\nEnter four integers: ";
+    std::cin >> a;
+    std::cout << "You entered the numbers: " << a;
+    std::cin >> a;
+    std::cout << " " <<  a;
+    std::cin >> a;
+    std::cout << " " << a;
+    std::cin >> a;
+    std::cout << " " << a;
+
+    cout << "\nEnter one integer and on integer number:";
+    cin >> c;
+    cout << c;
+    cin >> a;
+    cout << " " << a;
+
+
+
+
+
+
+    
+
+
+    
+
+ 
+  
+    
+
+   
+    
+    
+    
+
+    
+
+    
+   
+}
+
+
